cff3669b8a3d580141fcd05961cd80e0d0acc919
Rafael Guterres Jeffman <rafasgj@gmail.com>
2023-07-09 11:31:46 -0300

    Fix script to fix display organization.

--- Applications/display_autoorg.sh
+++ Applications/display_autoorg.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-join_list() {
+join_off_list() {
    first="${1-}"
    shift 1 2>/dev/null && printf -- "%s" "--output ${first}" "${@/#/ --off --output }" " --off"
 }
 
-off_list=($(xrandr | grep " disconnected" | cut -d " " -f 1))
-display_list=($(xrandr | grep " connected" | cut -d " " -f 1))
+off_list=($(xrandr | grep "disconnected" | cut -d " " -f 1 | sort))
+display_list=($(xrandr | grep "[^s]connected" | cut -d " " -f 1 | sort ))
 
-xrandr $(join_list "${off_list[@]}") $(join_list "${display_list[@]}")
+echo "OFF: ${off_list[@]}"
+echo "ON: ${display_list[@]}"
 
+[[ " ${display_list[@]} " =~ " eDP-1 " ]] && main="eDP-1" || main="${display_list[0]}"
+declare -a secondary
+for d in ${display_list[@]}; do
+    [ "${d}" == "${main}" ] || secondary+=( "${d}" )
+done
+echo "MAIN: ${main}"
+echo "SECONDARY: ${secondary[@]}"
+
+xrandr $(join_off_list "${off_list[@]}") $(join_off_list "${display_list[@]}")
+
+echo "Setting ${#display_list[@]} monitor(s): ${display_list[@]}"
 case ${#display_list[@]} in
     1)
-        echo "Setting monitor: ${display_list[0]}"
         xrandr --output ${display_list[0]} --auto
     ;;
     2)
-        echo "Setting 2 monitors: ${display_list[@]}"
-        if [ " ${disaplay_list[*]} " =~ " HMDI-1 " ]
-        then
-            xrandr --output eDP-1 --auto --output HDMI-1 --above eDP-1
-        else
-            xrandr --output eDP-1 --auto --output DP-2 --left-of eDP-1
-        fi
+        [[ " ${display_list[*]} " =~ " HDMI" ]] && position="--above" || position="--left-of"
+        xrandr --output ${main} --auto --output ${secondary[0]} --auto ${position} ${main}
     ;;
     3)
-        echo "Setting 3 monitors: ${display_list[@]}"
-        xrandr --output eDP-1 --auto --output HDMI-1 --auto --above eDP-1 --output DP-2 --auto --left-of HDMI-1
+        xrandr --output ${main} --auto --output ${secondary[1]} --auto --above ${main} --output ${secondary[0]} --auto --left-of ${secondary[1]}
     ;;
 esac
 
